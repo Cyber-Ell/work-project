e6740b0dba014afbf7c5ee58dc0282e6782735b0
Your Name <jeremiahagambo2020@.com>
2024-03-18 23:27:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+ 
+ 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('shown') }
+            else{
+                entry.target.classList.remove('shown') 
+            }
+        }
+    )
+})
+
+const hiddenElements = document.querySelectorAll('.fade-in');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Form script 
+
+const form = document.getElementById("form");
+form.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    fetch("https://getform.io/f/eapwddpb", {
+        method: "POST",
+        body: formData,
+        headers: {
+            "Accept": "application/json",
+        },
+    })
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+}
